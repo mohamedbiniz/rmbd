ea64d892f1f0667d9c59c7ad57fef019698f227f
pfleiss <pfleiss@prodell24.iwas.ifit.uni-klu.ac.at>
2012-03-16 10:11:46 +0100

    libaries updated

---
+++ owlcontroller/src/main/java/at/ainf/owlcontroller/OntologyDebugger.java
@@ -0,0 +1,14 @@
+package at.ainf.owlcontroller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pfleiss
+ * Date: 14.03.12
+ * Time: 14:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class OntologyDebugger {
+
+
+
+}
