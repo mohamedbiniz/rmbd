d151f0e51937c4a14987ddc75bf3ee9666e187d3
pfleiss <pfleiss@prodell24.iwas.ifit.uni-klu.ac.at>
2012-05-02 15:18:11 +0200

    cards

--- owlcontroller/src/test/java/at/ainf/owlcontroller/queryeval/UnsolvableTests.java
+++ owlcontroller/src/test/java/at/ainf/owlcontroller/queryeval/UnsolvableTests.java
@@ -318,6 +318,11 @@ public class UnsolvableTests extends BasePerformanceTests {
                 if (diagnoses.isEmpty())
                     logger.error("No diagnoses found!");
 
+                String infoCa = "";
+                for (Set<OWLLogicalAxiom> diagnose : diagnoses)
+                        infoCa += diagnose.size() + "/";
+                logger.info("cardinality of diagnoses " + infoCa);
+
                 // cast should be corrected
                 Iterator<AxiomSet<OWLLogicalAxiom>> descendSet = (new TreeSet<AxiomSet<OWLLogicalAxiom>>(diagnoses)).descendingIterator();
                 AxiomSet<OWLLogicalAxiom> d = descendSet.next();
@@ -852,8 +857,8 @@ public class UnsolvableTests extends BasePerformanceTests {
         //boolean background_add = false;
         showElRates = false;
 
-        String[] files = new String[]{"AgrMaker", "GOMMA-bk", "GOMMA-nobk", "Lily", "LogMap", "LogMapLt", "MapSSS"};
-        //String[] files = new String[]{"AgrMaker"};
+        //String[] files = new String[]{"AgrMaker", "GOMMA-bk", "GOMMA-nobk", "Lily", "LogMap", "LogMapLt", "MapSSS"};
+        String[] files = new String[]{"AgrMaker"};
 
         BasePerformanceTests.QSSType[] qssTypes = new BasePerformanceTests.QSSType[]{BasePerformanceTests.QSSType.MINSCORE, BasePerformanceTests.QSSType.SPLITINHALF, BasePerformanceTests.QSSType.DYNAMICRISK};
         for (boolean dual : new boolean[] {false}) {
