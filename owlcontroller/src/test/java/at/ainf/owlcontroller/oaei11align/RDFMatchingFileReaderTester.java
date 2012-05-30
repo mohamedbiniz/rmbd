587453f45691b801be9749aaa16af3ebcd793b53
pfleiss <pfleiss@prodell24.iwas.ifit.uni-klu.ac.at>
2012-05-30 15:22:55 +0200

    test anatomy

--- owlcontroller/src/test/java/at/ainf/owlcontroller/oaei11align/RDFMatchingFileReaderTester.java
+++ owlcontroller/src/test/java/at/ainf/owlcontroller/oaei11align/RDFMatchingFileReaderTester.java
@@ -252,7 +252,7 @@ public class RDFMatchingFileReaderTester {
             if (file.isDirectory())
                 continue;
 
-            SearchThread search = new SearchThread(file,false,d);
+            SearchThread search = new SearchThread(file,true,d);
 
             Future future = executor.submit(search);
         }
