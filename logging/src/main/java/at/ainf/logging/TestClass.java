cfb8cba8aa455758052285804cacaabad4a61d15
pfleiss <pfleiss@prodell24.iwas.ifit.uni-klu.ac.at>
2012-06-21 11:35:23 +0200

    removed jsolver

---
+++ logging/src/main/java/at/ainf/logging/TestClass.java
@@ -0,0 +1,16 @@
+package at.ainf.logging;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pfleiss
+ * Date: 20.06.12
+ * Time: 11:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestClass {
+
+    public void print() {
+        System.out.println("testclass Method ");
+    }
+
+}
