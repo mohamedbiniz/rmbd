34d54b2211b2991f9bf7774003ded8aa4a2f50ec
pfleiss <pfleiss@prodell24.iwas.ifit.uni-klu.ac.at>
2012-08-14 10:04:26 +0200

    modfied Aspects

--- logging/src/main/java/at/ainf/logging/TestAspect.java
+++ logging/src/main/java/at/ainf/logging/TestAspect.java
@@ -2,6 +2,7 @@ package at.ainf.logging;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -11,9 +12,9 @@ import org.slf4j.LoggerFactory;
 @Aspect
 public class TestAspect {
 
-    public static boolean ACTIVE = true;
+    //public static boolean ACTIVE = true;
 
-    @Pointcut("execution(void at.ainf.logging.TestClass.print(..))")
+    /*@Pointcut("execution(void at.ainf.logging.TestClass.print(..))")
     public static void mainCodeAspectPointcut() {
         //return ACTIVE;
     }
@@ -22,14 +23,17 @@ public class TestAspect {
     public void addMethod() {
         new ValueLogWatch(9,"diagnosis","number of diagnoses",
                 LoggerFactory.getLogger(ValueLogWatch.DEFAULT_LOGGER_NAME)).logValue();
-    }
+    }*/
 
-    @Before(value = "execution(* *(..)) && @annotation(profiled)", argNames = "pjp,profiled")
-    public void doPerfLogging(JoinPoint pjp, Profiled profiled) throws Throwable {
+    @Around(value = "execution(* *(..)) && @annotation(profiledVar)", argNames = "pjp,profiledVar")
+    public Object doPerfLogging(ProceedingJoinPoint pjp, ProfiledVar profiledVar) throws Throwable {
 
-        new ValueLogWatch(10,profiled.tag(),"number of diagnoses",
+        Object ret = pjp.proceed();
+
+        new ValueLogWatch((Integer)ret,profiledVar.varname(),"number of diagnoses",
                 LoggerFactory.getLogger(ValueLogWatch.DEFAULT_LOGGER_NAME)).logValue();
-        //return pjp.proceed();
+
+        return ret;
           }
 
 }
