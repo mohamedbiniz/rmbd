c69948682eda1d82d25ac650abe0af7db704e116
pfleiss <pfleiss@prodell24.iwas.ifit.uni-klu.ac.at>
2012-10-22 17:03:20 +0200

    IReasoner Sync in Constraint

--- owlapi-3/src/main/java/at/ainf/owlapi3/model/ReasonerOWL.java
+++ owlapi-3/src/main/java/at/ainf/owlapi3/model/ReasonerOWL.java
@@ -1,7 +1,13 @@
 package at.ainf.owlapi3.model;
 
 import at.ainf.diagnosis.model.AbstractReasoner;
-import org.semanticweb.owlapi.model.OWLLogicalAxiom;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,9 +18,50 @@ import org.semanticweb.owlapi.model.OWLLogicalAxiom;
  */
 public class ReasonerOWL extends AbstractReasoner<OWLLogicalAxiom> {
 
+    private OWLOntology ontology;
+
+    private OWLReasoner reasoner;
+
+    public ReasonerOWL(OWLOntologyManager owlOntologyManager, OWLReasonerFactory reasonerFactory) {
+        try {
+
+            OWLOntology dontology = owlOntologyManager.createOntology();
+            OWLLiteral lit = owlOntologyManager.getOWLDataFactory().getOWLLiteral("Test Reasoner Ontology ");
+            IRI iri = OWLRDFVocabulary.RDFS_COMMENT.getIRI();
+            OWLAnnotation anno = owlOntologyManager.getOWLDataFactory().getOWLAnnotation(owlOntologyManager.getOWLDataFactory().getOWLAnnotationProperty(iri), lit);
+            owlOntologyManager.applyChange(new AddOntologyAnnotation(dontology, anno));
+            this.ontology = dontology;
+
+            reasoner = reasonerFactory.createReasoner(this.ontology);
+        } catch (OWLOntologyCreationException e) {
+            throw new OWLRuntimeException(e);
+        }
+    }
+
     @Override
     public boolean isConsistent() {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
+        return false;
+    }
+
+    @Override
+    public void sync() {
+        Set<OWLLogicalAxiom> axiomsToAdd = new HashSet<OWLLogicalAxiom>();
+        Set<OWLLogicalAxiom> axiomsToRemove = new HashSet<OWLLogicalAxiom>();
+
+        for (OWLLogicalAxiom axiom : getReasonendFormulars()) {
+            if (!ontology.containsAxiom(axiom))
+                axiomsToAdd.add(axiom);
+        }
+
+        for (OWLLogicalAxiom axiom : ontology.getLogicalAxioms()) {
+            if (!getReasonendFormulars().contains(axiom))
+                axiomsToRemove.add(axiom);
+        }
+
+        if (!axiomsToAdd.isEmpty())
+            ontology.getOWLOntologyManager().addAxioms(ontology, axiomsToAdd);
+        if (!axiomsToRemove.isEmpty())
+            ontology.getOWLOntologyManager().removeAxioms(ontology, axiomsToRemove);
     }
 
 }
