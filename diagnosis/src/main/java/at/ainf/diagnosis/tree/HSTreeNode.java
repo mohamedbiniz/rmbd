836618bddcea6be8ae75e157ab10b347f6c15996
pfleiss <pfleiss@prodell24.iwas.ifit.uni-klu.ac.at>
2012-11-29 08:57:21 +0100

    rounded decimal exp orderprobab

--- diagnosis/src/main/java/at/ainf/diagnosis/tree/HSTreeNode.java
+++ diagnosis/src/main/java/at/ainf/diagnosis/tree/HSTreeNode.java
@@ -14,6 +14,9 @@ import at.ainf.diagnosis.storage.FormulaSet;
 import java.math.BigDecimal;
 import java.util.*;
 
+import static at.ainf.diagnosis.tree.Rounding.PRECISION;
+import static at.ainf.diagnosis.tree.Rounding.ROUNDING_MODE;
+
 /**
  * Created by IntelliJ IDEA.
  * User: student99
@@ -251,7 +254,7 @@ public class HSTreeNode<Id> implements Node<Id> {
                 Id axiom = getArcLabel();
                 BigDecimal fProb = getCostsEstimator().getFormulaCosts(axiom);
                 cost = BigDecimal.ONE.subtract(fProb);
-                cost = parent.getNodePathCosts().divide(cost);
+                cost = parent.getNodePathCosts().divide(cost,PRECISION,ROUNDING_MODE);
                 cost = cost.multiply(fProb);
             }
             this.nodePathCosts = cost;
