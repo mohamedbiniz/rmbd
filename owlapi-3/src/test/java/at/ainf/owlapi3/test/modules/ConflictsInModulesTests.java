e099ee60ec4e90c17f2d059c18d93ec8a2bda862
pfleiss <pfleiss@prodell24.iwas.ifit.uni-klu.ac.at>
2013-03-05 10:51:47 +0100

    module subset

--- owlapi-3/src/test/java/at/ainf/owlapi3/test/modules/ConflictsInModulesTests.java
+++ owlapi-3/src/test/java/at/ainf/owlapi3/test/modules/ConflictsInModulesTests.java
@@ -16,8 +16,12 @@ import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.io.ToStringRenderer;
 import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.reasoner.BufferingMode;
+import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
+import org.semanticweb.owlapi.reasoner.structural.StructuralReasoner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
 import uk.ac.manchester.cs.owl.owlapi.mansyntaxrenderer.ManchesterOWLSyntaxOWLObjectRendererImpl;
 
 import java.io.InputStream;
@@ -69,6 +73,109 @@ public class ConflictsInModulesTests {
 
     }
 
+    private static final OWLClass BOT_CLASS = OWLDataFactoryImpl.getInstance().getOWLNothing();
+
+    @Test
+    public void hierarchyOfUnsatclasses() throws OWLOntologyCreationException {
+        String onto = "fma2nci";
+        ToStringRenderer.getInstance().setRenderer(new ManchesterOWLSyntaxOWLObjectRendererImpl());
+        Set<OWLLogicalAxiom> onto1Axioms = getAxioms("ontologies/" + onto + "genonto1.owl");
+        Set<OWLLogicalAxiom> onto2Axioms = getAxioms("ontologies/" + onto + "genonto2.owl");
+        Set<OWLLogicalAxiom> mappingAxioms = getAxioms("ontologies/" + onto + "genmapp.owl");
+
+        Set<OWLLogicalAxiom> allAxioms = new HashSet<OWLLogicalAxiom>();
+        allAxioms.addAll(onto1Axioms);
+        allAxioms.addAll(onto2Axioms);
+        allAxioms.addAll(mappingAxioms);
+
+        OWLOntology fullOntology = createOntology(allAxioms);
+        Set<OWLClass> unsatClasses = new Reasoner.ReasonerFactory().createReasoner(fullOntology).getUnsatisfiableClasses().getEntitiesMinusBottom();
+        StructuralReasoner reasoner = new StructuralReasoner (fullOntology,
+                new SimpleConfiguration(), BufferingMode.NON_BUFFERING);
+        Map<OWLClass,Set<OWLClass>> map = new LinkedHashMap<OWLClass, Set<OWLClass>>();
+        for (OWLClass unsatClass : unsatClasses) {
+            Set<OWLClass> childs = new HashSet<OWLClass>(reasoner.getSubClasses(unsatClass,false).getFlattened());
+            childs.remove(BOT_CLASS);
+            if(!childs.isEmpty())map.put(unsatClass, childs);
+        }
+
+        for (OWLClass unsatClass : unsatClasses)
+            logger.info(unsatClass.toString() + ", " + getNumber(unsatClass));
+
+        for (OWLClass unsatClass : map.keySet()) {
+            String name = getNumber(unsatClass) + ": ";
+            for (Iterator<OWLClass> iterator = map.get(unsatClass).iterator(); iterator.hasNext(); ) {
+                name += getNumber(iterator.next());
+                if (iterator.hasNext())
+                    name += ", ";
+            }
+            logger.info(name);
+        }
+
+    }
+
+    public Integer getNumber (OWLClass unsatClass) {
+        if (!knownUnsatClasses.contains(unsatClass))
+            knownUnsatClasses.add(unsatClass);
+        return knownUnsatClasses.indexOf(unsatClass);
+
+    }
+
+    List<OWLClass> knownUnsatClasses = new LinkedList<OWLClass>();
+
+    @Test
+    public void findModulesSubsetOtherModules() throws OWLOntologyCreationException {
+
+        String onto = "fma2nci";
+        ToStringRenderer.getInstance().setRenderer(new ManchesterOWLSyntaxOWLObjectRendererImpl());
+        Set<OWLLogicalAxiom> onto1Axioms = getAxioms("ontologies/" + onto + "genonto1.owl");
+        Set<OWLLogicalAxiom> onto2Axioms = getAxioms("ontologies/" + onto + "genonto2.owl");
+        Set<OWLLogicalAxiom> mappingAxioms = getAxioms("ontologies/" + onto + "genmapp.owl");
+
+        Set<OWLLogicalAxiom> ontoAxioms = new HashSet<OWLLogicalAxiom>();
+        ontoAxioms.addAll(onto1Axioms);
+        ontoAxioms.addAll(onto2Axioms);
+
+        Set<OWLLogicalAxiom> allAxioms = new HashSet<OWLLogicalAxiom>();
+        allAxioms.addAll(ontoAxioms);
+        allAxioms.addAll(mappingAxioms);
+
+        OWLOntology fullOntology = createOntology(allAxioms);
+        OtfModuleProvider provider = new OtfModuleProvider(fullOntology, new Reasoner.ReasonerFactory(),false);
+        Set<OWLLogicalAxiom> bigModule = provider.getModuleUnsatClass();
+
+        Map<OWLClass, Set<OWLLogicalAxiom>> unsatMap = provider.getUnsatClasses();
+        Map<OWLClass,Set<OWLClass>> childMap = new HashMap<OWLClass, Set<OWLClass>>();
+
+        for (OWLClass unsatClass : unsatMap.keySet()) {
+            Set<OWLLogicalAxiom> module = unsatMap.get(unsatClass);
+            Set<OWLClass> childs = new LinkedHashSet<OWLClass>();
+
+            for (OWLClass child : unsatMap.keySet()) {
+                if (!child.equals(unsatClass) && module.containsAll(unsatMap.get(child)))
+                    childs.add(child);
+            }
+            childMap.put(unsatClass,childs);
+        }
+
+        for (OWLClass unsatClass : childMap.keySet())
+            logger.info(unsatClass.toString() + ", " + getNumber(unsatClass) + ", " + unsatMap.get(unsatClass).size());
+
+        for (OWLClass unsatClass : childMap.keySet()) {
+            String name = getNumber(unsatClass) + ": ";
+            for (Iterator<OWLClass> iterator = childMap.get(unsatClass).iterator(); iterator.hasNext(); ) {
+                OWLClass child = iterator.next();
+                name += getNumber(child);
+                if (iterator.hasNext())
+                    name += ", ";
+            }
+            logger.info(name);
+        }
+
+        logger.info("");
+
+    }
+
     @Test
     public void conflictsForEachModuleUsingBackground() throws OWLOntologyCreationException {
 
@@ -90,7 +197,19 @@ public class ConflictsInModulesTests {
         OtfModuleProvider provider = new OtfModuleProvider(fullOntology, new Reasoner.ReasonerFactory(),false);
         DistanceToTopComparator distanceToTopComparator = new DistanceToTopComparator(fullOntology);
         Set<OWLLogicalAxiom> bigModule = provider.getModuleUnsatClass();
-        Map<OWLClass,Set<OWLLogicalAxiom>> map = provider.getUnsatClasses();
+        Map<OWLClass,Set<OWLLogicalAxiom>> map = new LinkedHashMap<OWLClass, Set<OWLLogicalAxiom>>(
+                provider.getUnsatClasses());
+
+        /*Set<OWLClass> interleukins = new HashSet<OWLClass>();
+        for (OWLClass unsatClass : new LinkedHashSet<OWLClass>(map.keySet())) {
+            if (unsatClass.toString().equals("CC_chemokine"))
+                interleukins.add(unsatClass);
+        }
+        logger.info(interleukins.toString());
+        for (Iterator<OWLClass> i = map.keySet().iterator(); i.hasNext();) {
+            if (!i.next().equals(interleukins.iterator().next()))
+                i.remove();
+        }*/
 
         ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() - 1);
         Set<Future<String>> futures = new HashSet<Future<String>>();
@@ -202,7 +321,7 @@ public class ConflictsInModulesTests {
     private String getNumbersForConflicts(HsTreeSearch<FormulaSet<OWLLogicalAxiom>, OWLLogicalAxiom> search) {
         String result = "";
         for (Set<OWLLogicalAxiom> conflict : search.getConflicts()) {
-            result += getNumber(conflict) + "~";
+            result += getNumber(conflict) + "(" + conflict.size() + ")~";
         }
         return result;
     }
