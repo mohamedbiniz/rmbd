b93dca2e2e1faa644f296257b1f703ab4697076d
kostyantyn shchekotykhin <kostyantyn.shchekotykhin@gmail.com>
2013-05-15 11:30:48 +0200

    core extraction

--- owlapi-3/src/main/java/at/ainf/owlapi3/reasoner/cores/HornCore.java
+++ owlapi-3/src/main/java/at/ainf/owlapi3/reasoner/cores/HornCore.java
@@ -1,13 +1,7 @@
 package at.ainf.owlapi3.reasoner.cores;
 
 import at.ainf.owlapi3.reasoner.HornSatReasoner;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
 import org.sat4j.specs.IVecInt;
-import org.sat4j.specs.IteratorInt;
-import org.semanticweb.owlapi.model.OWLClass;
-
-import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA.
@@ -27,29 +21,11 @@ public class HornCore extends Core {
         super(reasoner);
     }
 
-    protected HornCore extractCore(Integer literal, int level) {
-        // remove negation
-        int symbol = Math.abs(literal);
-        if (getSymbolsSet().contains(symbol)) return this;
-        symbols.put(symbol, level);
-        //if (core.selectedClasses.contains(symbol))
-        //    core.selectedScore++;
-        // analyze clauses in which symbol is positive, i.e. in the head of a rule
-        for (IVecInt clause : getReasoner().getSymbolsToClauses().get(symbol)) {
-            if (!getReasoner().getSolverClauses().containsKey(clause))
-                continue;
-            final boolean isHornClause = isHornClause(clause);
-            if (!isHornClause) isHornComplete = false;
-            if (!isHornClause) {
-                continue;
-            }
-            //Set<Integer> neg = containsAllNegativeSymbols(clause, false);
-
-            for (IteratorInt iterator = clause.iterator(); iterator.hasNext(); ) {
-                int lit = iterator.next();
-                if (lit < 0) extractCore(lit, ++level);
-            }
+    protected boolean isExpandable(IVecInt clause) {
+        if (!isBCHornClause(clause)) {
+            isHornComplete = false;
+            return false;
         }
-        return this;
+        return true;
     }
 }
