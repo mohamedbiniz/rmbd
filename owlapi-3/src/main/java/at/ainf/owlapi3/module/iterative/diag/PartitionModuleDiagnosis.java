cbb379f75765457aca699f2073afbfee7c097c6b
pr8 <pr8@prodell30.iwas.ifit.uni-klu.ac.at>
2013-05-16 18:46:37 +0200

    partition module diagnosis (started)

--- owlapi-3/src/main/java/at/ainf/owlapi3/module/iterative/diag/PartitionModuleDiagnosis.java
+++ owlapi-3/src/main/java/at/ainf/owlapi3/module/iterative/diag/PartitionModuleDiagnosis.java
@@ -1,10 +1,16 @@
 package at.ainf.owlapi3.module.iterative.diag;
 
+import at.ainf.diagnosis.Speed4JMeasurement;
+import at.ainf.owlapi3.model.OWLModuleExtractor;
 import at.ainf.owlapi3.module.iterative.ModuleDiagSearcher;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLLogicalAxiom;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,13 +21,69 @@ import java.util.Set;
  */
 public class PartitionModuleDiagnosis extends AbstractRootModuleDiagnosis {
 
+    private static Logger logger = LoggerFactory.getLogger(RootModuleDiagnosis.class.getName());
+
+    private final int MAX_REDUCTION_TRIALS = 5;
+    private final int MAX_MODULESIZE_FOR_DEBUG = 500;
+    private Set<Set<OWLLogicalAxiom>> atoms = new LinkedHashSet<Set<OWLLogicalAxiom>>();
+
     public PartitionModuleDiagnosis(Set<OWLLogicalAxiom> mappings, Set<OWLLogicalAxiom> ontoAxioms, OWLReasonerFactory factory, ModuleDiagSearcher moduleDiagSearcher) {
         super(mappings, ontoAxioms, factory, moduleDiagSearcher);
     }
 
+    protected Set<OWLLogicalAxiom> createFixpointModule (Set<OWLLogicalAxiom> ontology, int nParts) {
+
+        /*List<OWLClass> classesInModuleSignature = new LinkedList<OWLClass>(getClassesInModuleSignature(ontology));
+        List<Set<OWLLogicalAxiom>> modules = recursiveModuleExtract(ontology, classesInModuleSignature, nParts);
+        Set<OWLLogicalAxiom> intersection = createIntersection(modules);
+
+        logger.info ("intersection size / intersection signature size / expressiveness intersection: " + intersection.size() + ", " + getClassesInModuleSignature(intersection).size() + ", " + getExpressivity(intersection));
+
+        if (intersection.size() == ontology.size())
+            return intersection;
+
+        return createFixpointModule(intersection, nParts);*/
+        return null;
+    }
+
+    protected List<Set<OWLLogicalAxiom>> recursiveModuleExtract (Set<OWLLogicalAxiom> ontology, List<OWLClass> signature, int nParts) {
+
+        List<Set<OWLLogicalAxiom>> submodules = new LinkedList<Set<OWLLogicalAxiom>>();
+        OWLModuleExtractor extractor = new OWLModuleExtractor(ontology);
+        List<List<OWLClass>> subsignatures = calculateSubSignaturePartitioned (signature, nParts);
+        for (List<OWLClass> s : subsignatures) {
+            Speed4JMeasurement.start("submodules_extraction_single");
+            Set<OWLLogicalAxiom> submod = extractor.calculateModule(s);
+            long time = Speed4JMeasurement.stop();
+            //logger.info("parts / time / start signature size / submodul size / signature size: " +
+            //        split + ", " + time + ", " + s.size() + ", " + submod.size() + ", " + getClassesInModuleSignature(submod).size());
+            submodules.add(submod);
+        }
+        return submodules;
+    }
+
+    private List<List<OWLClass>> calculateSubSignaturePartitioned (List<OWLClass> signature, int nParts){
+        return null;
+    }
+
+
     @Override
     public Set<OWLLogicalAxiom> calculateTargetDiagnosis() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        //Set<OWLLogicalAxiom> fixpointModule = createFixpointModule();
+
+        //first: partition-intersection reduction until fixpoint
+        // if fixpoint-module has size higher than MAX_SIZE_FOR_DEBUG, then
+        //second: reduce to root until module is smaller than MAX_SIZE_FOR_DEBUG
+        //
+        return null;
+    }
+
+    protected Set<OWLLogicalAxiom> reduceToRootModule(Set<OWLLogicalAxiom> module, Set<OWLClass> atom) {
+        ArrayList<OWLClass> moduleSignature = new ArrayList<OWLClass>(getClassesInModuleSignature(module));
+        int moduleSignatureSize = moduleSignature.size();
+        int classIndex = (int) Math.floor(Math.random());
+        OWLClass nextClass = moduleSignature.get(classIndex);
+        return null;
     }
 
 }
