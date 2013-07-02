91d389c0bba6bb05eff383ebcf56525ed1f71283
philipp fleiss <philipp.fleiss@gmx.at>
2013-07-02 13:45:39 +0200

    corrected OAEI11conference

--- asp/src/main/java/at/ainf/asp/model/ASPOutput.java
+++ asp/src/main/java/at/ainf/asp/model/ASPOutput.java
@@ -1,44 +1,40 @@
+/**
+ * 
+ */
 package at.ainf.asp.model;
 
-/**
- * Created with IntelliJ IDEA.
- * User: pfleiss
- * Date: 10.06.13
- * Time: 14:46
- * To change this template use File | Settings | File Templates.
- */
+
 /**
  * @author Melanie Fruehstueck
  *
  */
 public class ASPOutput {
 
-    protected static ASPOutput output;
+	protected static ASPOutput output;
+	
+	private boolean isSatisfiable;
+	private boolean isUnknown;
+	
+	public static ASPOutput getASPOutputInstance() {
+		if (output == null) {
+			output = new ASPOutput();
+		}
+		return output;
+	}
+	
+	public boolean isSatisfiabl() {
+		return isSatisfiable;
+	}
+	
+	public void setSatisfiable(boolean isSatisfiable) {
+		this.isSatisfiable = isSatisfiable;
+	}
 
-    private boolean isSatisfiable;
-    private boolean isUnknown;
+	public boolean isUnknown() {
+		return isUnknown;
+	}
 
-    public static ASPOutput getASPOutputInstance() {
-        if (output == null) {
-            output = new ASPOutput();
-        }
-        return output;
-    }
-
-    public boolean isSatisfiabl() {
-        return isSatisfiable;
-    }
-
-    public void setSatisfiable(boolean isSatisfiable) {
-        this.isSatisfiable = isSatisfiable;
-    }
-
-    public boolean isUnknown() {
-        return isUnknown;
-    }
-
-    public void setUnknown(boolean isUnknown) {
-        this.isUnknown = isUnknown;
-    }
-
+	public void setUnknown(boolean isUnknown) {
+		this.isUnknown = isUnknown;
+	}
 }
